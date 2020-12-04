efa2c011f9c4e37a8dca880fd6239bc814515607
DanHodges <dhdgs690@gmail.com>
2020-12-04 13:47:05 -0600

    capital T the try

--- src/main/scala/aoc_2020_dotty/DayFour.scala
+++ src/main/scala/aoc_2020_dotty/DayFour.scala
@@ -1,5 +1,7 @@
 package aoc_2020_dotty
 
+import scala.util.Try
+
 object DayFour extends Day:
   def data(input: String): Seq[Map[String, String]] =
     input.split("\\n\\n").toSeq.filterNot(_.isEmpty)
@@ -17,21 +19,21 @@ object DayFour extends Day:
                       pid: String,
                       cid: Option[String]) {
     
-    def validByr:Boolean = byr.intCheck(_.isWithin(1920, 2002))
-    def validIyr:Boolean = iyr.intCheck(_.isWithin(2010, 2020))
-    def validEyr:Boolean = eyr.intCheck(_.isWithin(2020, 2030))
+    def validByr: Boolean = byr.intCheck(_.isWithin(1920, 2002))
+    def validIyr: Boolean = iyr.intCheck(_.isWithin(2010, 2020))
+    def validEyr: Boolean = eyr.intCheck(_.isWithin(2020, 2030))
     
-    def validHgt:Boolean = (hgt.dropRight(2), hgt.takeRight(2)) match
+    def validHgt: Boolean = (hgt.dropRight(2), hgt.takeRight(2)) match
       case (height, "cm") => height.intCheck(_.isWithin(150, 193))
       case (height, "in") => height.intCheck(_.isWithin(59, 76))
       case _ => false
     
-    def validHcl:Boolean = hcl.toList match
-      case head :: tail if tail.size == 6 && head == '#' => tail.toSet.subsetOf("0123456789abcdef".toSet)
+    def validHcl: Boolean = hcl.toList match
+      case head :: tail if head == '#' && tail.size == 6 => tail.toSet.subsetOf("0123456789abcdef".toSet)
       case _ => false
     
     
-    def validEcl = Set("amb", "blu", "brn","gry","grn","hzl","oth").contains(ecl)
+    def validEcl = Set("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(ecl)
     def validPid = pid.size == 9 && pid.intCheck(x => true)
     
     def isValid: Boolean = Seq(validByr, validIyr, validEyr, validHgt, validHcl, validEcl, validPid).forall(identity)
@@ -39,7 +41,8 @@ object DayFour extends Day:
   
   object Passport:
     def apply(m: Map[String, String]): Option[Passport] =
-      try Some(new Passport(
+      Try {
+        Some(new Passport(
           byr = m("byr"),
           iyr = m("iyr"),
           eyr = m("eyr"),
@@ -49,23 +52,20 @@ object DayFour extends Day:
           pid = m("pid"),
           cid = m.get("cid"),
         ))
-      catch case e: Exception => None
+      }.getOrElse(None)
       
   implicit class RichInt(val i: Int):
     def isWithin(a: Int, b: Int) = i >= a && i <= b
   
   implicit class RichString(val s: String):
-    def intCheck(condition: Int => Boolean):Boolean =
-      try 
-        val int = Integer.parseInt(s)
-        condition(int)
-      catch case e: Exception => false
+    def intCheck(condition: Int => Boolean): Boolean =
+      Try { condition(s.toInt) }.getOrElse(false)
     
       
-  def partOne(s: String):Int =
+  def partOne(s: String): Int =
     data(s).flatMap(Passport.apply).size
     
-  def partTwo(s: String):Int =
+  def partTwo(s: String): Int =
     data(s).flatMap(Passport.apply).filter(_.isValid).size
 
 
